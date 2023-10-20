718774c9552a391fd6039d40b3a00134851b1322
yashikaRupasinghe <yashika.rupasinghe@gmail.com>
2023-10-20 21:39:28 +0530

    loaded the model

--- predict_page.py
+++ predict_page.py
@@ -2,3 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import pickle as pk
+
+# with open('linear_regression_model.pkl', 'rb') as model_file:
+#    linear_regression_model = pk.load(model_file)
+
+with open('random_forest_model.pkl', 'rb') as model_file:
+   random_forest_model = pk.load(model_file)
