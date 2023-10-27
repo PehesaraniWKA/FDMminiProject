9e4d43ba2b4c7ef9b2826d5226efc181402a0fc2
PehesaraniWKA <it21259470@my.sliit.lk>
2023-10-27 23:23:43 +0530

    file modified

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 import streamlit as st
+import pickle as pk
 from predict_page import show_predict_page
 from explore_page import show_explore_page
+
+# try:
+#     from predict_page import show_predict_page
+#     random_forest_model = pk.load('random_forest_model.pkl', 'rb')
+# except ModuleNotFoundError:
+#     print("The 'predict_page' module could not be found.")
+# except FileNotFoundError:
+#     print(f"The model file does not exist.")
+
 
 page = st.sidebar.selectbox("Explore or Predict", ("Predict","Explore"))
 
